d3b76b412f46a9c7ebeae782d6ee7b22f5bc02c8
jmoerk123 <jmoerk0101@gmail.com>
2025-01-25 20:05:09 +0100

    added calculation of curve length and amount of cuts

--- app/main.py
+++ app/main.py
@@ -17,10 +17,20 @@ async def read_form(request: Request):
 
 
 @app.post("/calculate", response_class=HTMLResponse)
-async def calculate_sum(request: Request, b: float = Form(...), h: float = Form(...)):
-    result_rad = math.atan(b / h)
+async def calculate_sum(
+    request: Request,
+    cut_width: float = Form(...),
+    cut_depth: float = Form(...),
+    radius: float = Form(...),
+    total_deg: float = Form(...),
+):
+    result_rad = math.atan(cut_width / cut_depth)
     result_deg = result_rad * (180 / math.pi)
     cuts_pr_90 = 90 / result_deg
+    curve_ratio = 360 / total_deg
+    print(curve_ratio)
+    curve_len = (radius * 2 * math.pi) / curve_ratio
+    num_cuts = total_deg / result_deg
     return templates.TemplateResponse(
         "index.html",
         {
@@ -28,5 +38,7 @@ async def calculate_sum(request: Request, b: float = Form(...), h: float = Form(
             "result_deg": f"{result_deg:.2f}",
             "result_rad": f"{result_rad:.2f}",
             "cuts_pr_90": f"{cuts_pr_90:.2f}",
+            "curve_len": f"{curve_len:.2f}",
+            "num_cuts": f"{num_cuts:.2f}",
         },
     )
